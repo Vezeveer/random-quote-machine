6a4b8d206dfe655c8bb10d5fe9b2fbe92ad512c7
vezeveer <mortalmanny@live.com>
2019-02-28 14:06:18 +0800

    Updates

---
+++ precache-manifest.e291306f680b45ea38aa61e29f893350.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7645c56e908f98c93ec",
+    "url": "/Vezeveer/random-quote-machine/static/css/main.c40bc778.chunk.css"
+  },
+  {
+    "revision": "a7645c56e908f98c93ec",
+    "url": "/Vezeveer/random-quote-machine/static/js/main.a7645c56.chunk.js"
+  },
+  {
+    "revision": "49f23248fc37c28c89ea",
+    "url": "/Vezeveer/random-quote-machine/static/js/runtime~main.49f23248.js"
+  },
+  {
+    "revision": "0c8fa71a6577f3fb1039",
+    "url": "/Vezeveer/random-quote-machine/static/css/2.2d3b02f1.chunk.css"
+  },
+  {
+    "revision": "0c8fa71a6577f3fb1039",
+    "url": "/Vezeveer/random-quote-machine/static/js/2.0c8fa71a.chunk.js"
+  },
+  {
+    "revision": "ca2b1d18330858224790907d93b3db06",
+    "url": "/Vezeveer/random-quote-machine/index.html"
+  }
+];
